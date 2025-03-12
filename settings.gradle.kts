d4e3668562a4dcbad5f6a86ed33e01062c986673
Andrés Quesada <aquesadaa1@ucenfotec.ac.cr>
2025-03-11 18:42:34 -0600

    Proyecto base

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "BISOFT-38-proyecto-04-actividad-02"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
